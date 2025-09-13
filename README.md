4bf43229b1456aec5c4960b5c2116f7f9ecf3d2d
Eirik <eirinore@uio.no>
2025-09-13 17:21:02 +0200

    start coding the skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+A Transport Services implementation as described in RFC 9622, in Rust
---
+++ src/transport_properties/selection_properties.rs
@@ -0,0 +1,44 @@
+enum Preference {
+    Require,
+    Prefer,
+    NoPreference,
+    Avoid,
+    Prohibit,
+}
+
+struct TransportProperties {
+    reliability: Preference,
+    preserve_message_boundaries: Preference,
+    per_msg_reliability: Preference,
+    preserve_order: Preference,
+    zero_rtt_msg: Preference,
+    multistreaming: Preference,
+    full_checksum_send: Preference,
+    full_checksum_recv: Preference,
+    congestion_control: Preference,
+    keep_alive: Preference,
+    interface: Vec<(Preference, i32)>, // Temporary, need to figure out what type to use
+}
+
+impl SelectionProperties {
+    /* Constructor for the SelectionProperties struct. Default values are as described
+     * in RFC9622
+     */
+    pub fn new() {
+        SelectionProperties {
+            reliability: Require,
+            preserve_message_boundaries: NoPreference,
+            per_msg_reliability: NoPreference,
+            preserve_order: Require,
+            zero_rtt_msg: NoPreference,
+            multistreaming: Prefer,
+            full_checksum_send: Require,
+            full_checksum_recv: Require,
+            congestion_control: Require,
+            keep_alive: NoPreference,
+            interface: Vec::new(),
+        }
+    }
+
+    pub fn set(&mut self, property: String, value: Preference) {}
+}
