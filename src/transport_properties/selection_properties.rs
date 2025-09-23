df1c2373f354b773b3615cd1b4debbcf91cecb4e
Eirik <eirinore@uio.no>
2025-09-23 19:06:22 +0200

    change name of an enum

--- src/transport_properties/selection_properties.rs
+++ src/transport_properties/selection_properties.rs
@@ -19,7 +19,7 @@ pub enum Direction {
 // This enumeration allows SelectionProperties.set(...) to take any of the
 // types of value as an argument wrapped in the enumeration
 #[derive(Debug)]
-pub enum SelectionPropertyType {
+pub enum SelectionPropertyValue {
     Preference(Preference),
     InterfaceSet(HashSet<(Preference, i32)>), // i32 subject to change
     PvdSet(HashSet<(Preference, i32)>),       // Same here
@@ -86,129 +86,129 @@ impl SelectionProperties {
         // Maybe this as well
     }
 
-    pub fn set(&mut self, property: &str, value: SelectionPropertyType) {
+    pub fn set(&mut self, property: &str, value: SelectionPropertyValue) {
         match property.replace("_", "").to_lowercase().as_str() {
             "reliability" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.reliability = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "preservemessageboundaries" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.preserve_message_boundaries = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "permsgreliability" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.per_msg_reliability = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "preserveorder" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.preserve_order = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "zerorttmsg" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.zero_rtt_msg = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "multistreaming" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.multistreaming = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "fullchecksumsend" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.full_checksum_send = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "fullchecksumrecv" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.full_checksum_recv = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "congestioncontrol" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.congestion_control = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "keepalive" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.keep_alive = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "interface" => {
-                if let SelectionPropertyType::InterfaceSet(preference_set) = value {
+                if let SelectionPropertyValue::InterfaceSet(preference_set) = value {
                     self.interface = preference_set;
                 } else {
                     // Should throw an error
                 }
             }
             "pvd" => {
-                if let SelectionPropertyType::PvdSet(preference_set) = value {
+                if let SelectionPropertyValue::PvdSet(preference_set) = value {
                     self.pvd = preference_set;
                 } else {
                     // Should throw an error
                 }
             }
             "usetemporarylocaladdress" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.use_temporary_local_address = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "multipath" => {
-                if let SelectionPropertyType::MultipathPreference(multipath_preference) = value {
+                if let SelectionPropertyValue::MultipathPreference(multipath_preference) = value {
                     self.multipath = multipath_preference;
                 } else {
                     // Should throw an error
                 }
             }
             "advertisesaltaddr" => {
-                if let SelectionPropertyType::Bool(preference) = value {
+                if let SelectionPropertyValue::Bool(preference) = value {
                     self.advertises_altaddr = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "direction" => {
-                if let SelectionPropertyType::Direction(direction) = value {
+                if let SelectionPropertyValue::Direction(direction) = value {
                     self.direction = direction;
                 } else {
                     // Should throw an error
                 }
             }
             "softerrornotify" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.soft_error_notify = preference;
                 } else {
                     // Should throw an error
                 }
             }
             "activereadbeforesend" => {
-                if let SelectionPropertyType::Preference(preference) = value {
+                if let SelectionPropertyValue::Preference(preference) = value {
                     self.active_read_before_send = preference;
                 } else {
                     // Should throw an error
@@ -221,35 +221,35 @@ impl SelectionProperties {
     pub fn require(&mut self, property: &str) {
         self.set(
             property,
-            SelectionPropertyType::Preference(Preference::Require),
+            SelectionPropertyValue::Preference(Preference::Require),
         );
     }
 
     pub fn prefer(&mut self, property: &str) {
         self.set(
             property,
-            SelectionPropertyType::Preference(Preference::Prefer),
+            SelectionPropertyValue::Preference(Preference::Prefer),
         );
     }
 
     pub fn no_preference(&mut self, property: &str) {
         self.set(
             property,
-            SelectionPropertyType::Preference(Preference::NoPreference),
+            SelectionPropertyValue::Preference(Preference::NoPreference),
         );
     }
 
     pub fn avoid(&mut self, property: &str) {
         self.set(
             property,
-            SelectionPropertyType::Preference(Preference::Avoid),
+            SelectionPropertyValue::Preference(Preference::Avoid),
         );
     }
 
     pub fn prohibit(&mut self, property: &str) {
         self.set(
             property,
-            SelectionPropertyType::Preference(Preference::Prohibit),
+            SelectionPropertyValue::Preference(Preference::Prohibit),
         );
     }
 }
@@ -303,7 +303,7 @@ mod tests {
         assert_eq!(NoPreference, selection_properties.active_read_before_send);
 
         // Testing setting a Preference type
-        selection_properties.set("reliability", SelectionPropertyType::Preference(Avoid));
+        selection_properties.set("reliability", SelectionPropertyValue::Preference(Avoid));
         assert_eq!(Avoid, selection_properties.reliability);
 
         // Testing setting a vector of tuples
@@ -312,7 +312,7 @@ mod tests {
         interface_preference.insert((Prohibit, 2));
         selection_properties.set(
             "interface",
-            SelectionPropertyType::InterfaceSet(interface_preference),
+            SelectionPropertyValue::InterfaceSet(interface_preference),
         );
         println!(
             "This should be [(prefer, 5), (prohibit, 2):\n{:#?}",
@@ -322,18 +322,18 @@ mod tests {
         // Testing setting a multipath preference
         selection_properties.set(
             "multipath",
-            SelectionPropertyType::MultipathPreference(MultipathPreference::Active),
+            SelectionPropertyValue::MultipathPreference(MultipathPreference::Active),
         );
         assert_eq!(MultipathPreference::Active, selection_properties.multipath);
 
         // Testing setting a bool
-        selection_properties.set("advertisesaltaddr", SelectionPropertyType::Bool(true));
+        selection_properties.set("advertisesaltaddr", SelectionPropertyValue::Bool(true));
         assert_eq!(true, selection_properties.advertises_altaddr);
 
         // Testing setting a direction
         selection_properties.set(
             "direction",
-            SelectionPropertyType::Direction(Direction::UnidirectionalSend),
+            SelectionPropertyValue::Direction(Direction::UnidirectionalSend),
         );
         assert_eq!(
             Direction::UnidirectionalSend,
