15e1f603f661f8e9617721af7f95bdf46d6930f6
Eirik <eirinore@uio.no>
2025-09-23 19:52:33 +0200

    this is useless, don't bother looking

---
+++ src/security_parameters.rs
@@ -0,0 +1,107 @@
+// This is a sekelton of security parameters: I am clueless here so this
+// might be deleted, who knows
+// Which means: NOTHING OF NOTE HERE, you can stop reading
+// Why are you still reading, there is nothing here
+
+use std::collections::HashSet;
+
+// Placeholder for security protocols
+enum SecurityProtocol {
+    Tls1_2,
+    Tls1_3,
+}
+
+// Placeholders for certificates
+struct Certificate {
+    member_1: i32,
+    member_2: u32,
+}
+
+// Placeholder for pinned server certificate
+struct CertificateChain {
+    blabla_1: u32,
+    blabla_2: i32,
+}
+
+// Another placeholder
+enum SupportedGroup {
+    Secp256r1,
+}
+
+// Yet another placeholder
+enum Ciphersuite {
+    TlsAes128GcmSha256,
+}
+
+// You guessed it, a placeholder
+enum SignatureAlgorithm {
+    Bliipbloop,
+}
+
+// Do I need to say this every time
+enum SecurityParameterValue {
+    ProtocolSet(HashSet<SecurityProtocol>),
+    CertificateArray(Vec<Certificate>),
+}
+
+// Probably not
+struct PreSharedKey {
+    key: String,
+    identity: String,
+}
+
+// Oh well
+struct CallBack {
+    something: String,
+}
+
+// Why are you still here
+impl CallBack {
+    pub fn new() -> Self {
+        Self {
+            something: String::from("BAH"),
+        }
+    }
+}
+
+struct SecurityParameters {
+    allowed_security_protocols: HashSet<SecurityProtocol>,
+    server_certificate: Vec<Certificate>,
+    client_certificate: Vec<Certificate>,
+    pinned_server_certificate: Vec<CertificateChain>,
+    alpn: Vec<String>,
+    supported_group: Vec<SupportedGroup>,
+    ciphersuite: Vec<Ciphersuite>,
+    signature_algorithm: Vec<SignatureAlgorithm>,
+    max_cached_sessions: i32,
+    cached_session_lifetime_seconds: i32,
+    pre_shared_key: PreSharedKey,
+}
+
+impl SecurityParameters {
+    // Constructor for the security parameters with default values
+    // as given in RFC 9622
+    pub fn new() {
+        //
+    }
+
+    pub fn new_disabled_security_parameters() {
+        //
+    }
+
+    pub fn new_opportunistic_security_parameters() {
+        //
+    }
+
+    pub fn set() {
+        // Set members
+    }
+
+    pub fn set_trust_verification_callback(trust: CallBack) {
+        // Bah
+    }
+
+    pub fn set_identity_challenge_callback(challenge: CallBack) {
+        // Bingbong
+    }
+}
